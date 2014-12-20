879000fb1ef91d1dc773b1261f4508b3fa98e3de
furyofdragon <furyofdragon@gmail.com>
2014-12-20 05:16:17 +0400

    all variables in RDZY declared

--- RAVNUS/src/rdzy.for
+++ RAVNUS/src/rdzy.for
@@ -1,8 +1,11 @@
       SUBROUTINE RDZY(EL,FILE,PROEKT,ZY,IR)
+      IMPLICIT NONE
       REAL(4)      ZY(999)
       CHARACTER*64 FILE
       CHARACTER*64 PROEKT
       REAL(4)      EL
+      INTEGER(4)   IR
+      INTEGER(4)   K
 10    WRITE(*,'(A)') '“Š€†ˆ’… ˆŒŸ ”€‰‹€ ‘ ˆ”ŽŒ€–ˆ…‰ Ž ”ŽŒ… ŠŽ“‘€:'
       READ(*,'(A)', ERR=10, END=11) FILE
       OPEN(11,FILE=FILE)
@@ -10,6 +13,7 @@
       READ(11,'(A)', ERR=4) PROEKT
       READ(11,    *, ERR=4) EL
       DO K = 1, 999
+!TO DO What if file length bigger then 999 ?
           READ(11, *, ERR=4, END=4) ZY(K)
           IF(ZY(K).GT.998.) exit
       END DO
