2e237ef46e488e06a89e2882315027c0e2596972
furyofdragon <furyofdragon@gmail.com>
2015-02-04 03:50:58 +0400

    code format

--- RAVNUS/src/rdzy.for
+++ RAVNUS/src/rdzy.for
@@ -1,6 +1,6 @@
       SUBROUTINE RDZY(EL,FILE,PROEKT,ZY,IR)
       IMPLICIT NONE
-      REAL(4)        ZY(999)
+      REAL(4), ALLOCATABLE :: ZY(:)
       CHARACTER(256) FILE
       CHARACTER(256) PROEKT
       REAL(4)        EL
@@ -14,18 +14,19 @@
       READ(11,'(A)', ERR=4) PROEKT
       READ(11,    *, ERR=4) EL
       CALL READ_EOF(11, nstrok)
+      allocate(ZY(nstrok))
       DO K = 1, nstrok
           READ(11, *, ERR=4, END=4) ZY(K)
           IF(ZY(K).GT.998.) exit
       END DO
       CLOSE(11)
       ! ¬®¤¨δ¨ª ζ¨ο δ®ΰ¬λ ε-ª®®ΰ¤¨­ β ª®ΰ―γα 
-      CALL XYZP(EL,ZY)
+      CALL XYZP(nstrok, EL,ZY)
       RETURN
 
 4     CONTINUE
-      WRITE(*, '(A)') '‚ ”€–  ”… “‘€ €“†…› .'
-      WRITE(*, '(A,/)')' „‹ „‹†… €’› €†’… <RETURN>'
+      WRITE(*, '(A)')  '‚ ”€–  ”… “‘€ €“†…› .'
+      WRITE(*, '(A,/)')'„‹ „‹†… €’› €†’… <RETURN>'
       READ(*,*)
 11    IR=2
       STOP
