5bc4921eb82b33c1cb32adfa67dd3db67295ed9e
furyofdragon <furyofdragon@gmail.com>
2015-02-05 02:21:13 +0400

    code simplified

--- RAVNUS/src/RDZY.for
+++ RAVNUS/src/RDZY.for
@@ -12,15 +12,15 @@
       READ(11, '(A)', ERR = 4) PROEKT
       READ(11, *, ERR = 4) EL
       DO 6 K = 1, 999
-      READ(11, *, ERR = 4, END = 4) ZY(K)
-      IF(ZY(K).LT.332.) GO TO 35
-      IF(ZY(K).GT.998.) GO TO 8
-      IF(MOD(KT,2).NE.0) GO TO 4
-      KT = 1
-      GO TO 6
-35    KT = KT + 1
+          READ(11, *, ERR = 4, END = 4) ZY(K)
+          IF(ZY(K).LT.332.) GO TO 35
+          IF(ZY(K).GT.998.) exit
+          IF(MOD(KT,2).NE.0) GO TO 4
+          KT = 1
+          GO TO 6
+35        KT = KT + 1
 6     CONTINUE
-8     CLOSE(11)
+      CLOSE(11)
       CALL XYZP(EL, ZY)
       RETURN
 
