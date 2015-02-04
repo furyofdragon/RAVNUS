96d84f6ef5c15faab05fa2b04bd91c71fe6dfe31
furyofdragon <furyofdragon@gmail.com>
2015-02-05 02:26:57 +0400

    code format

--- RAVNUS/src/XYZP.for
+++ RAVNUS/src/XYZP.for
@@ -1,19 +1,18 @@
-	SUBROUTINE XYZP(EL,ZY)
+      SUBROUTINE XYZP(EL, ZY)
       DIMENSION ZY(999)
-      DL=EL/20.
-      EL2=EL/2.
-      I=1
-      J=1
-1     ZY(I)=(EL2-ZY(J))/DL+100.
-2     J=J+1
-      I=I+1
-      IF(ZY(J).GT.332.AND.ZY(J).LT.998.)GO TO 3
-      IF(ZY(J).GT.998.)GO TO 4
-      ZY(I)=ZY(J)
+      DL  = EL/20.
+      EL2 = EL/2.
+      I = 1
+      J = 1
+1     ZY(I) = (EL2 - ZY(J))/DL + 100.
+2     J = J + 1
+      I = I + 1
+      IF(ZY(J).GT.332.AND.ZY(J).LT.998.) GO TO 3
+      IF(ZY(J).GT.998.) GO TO 4
+      ZY(I) = ZY(J)
       GO TO 2
-3     J=J+1
-      IF(ZY(J).LT.999.)GOTO 1
-4     ZY(I)=ZY(J)
+3     J = J + 1
+      IF(ZY(J).LT.999.) GO TO 1
+4     ZY(I) = ZY(J)
       RETURN
       END
-
